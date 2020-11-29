c3b9b52939a6557cfbe34eef628d25e3e879faf0
Syed Muhammed Bilal <intergo878@gmail.com>
2020-11-29 11:44:28 +0500

    new commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [value, setValue] = useState('');
+
   return (
     <React.Fragment>
       <h1>Todo App</h1>
+      <form>
+        <input type='text' />
+        <button type='submit'></button>
+      </form>
     </React.Fragment>
   )
-};
+}
 
 export default App;
