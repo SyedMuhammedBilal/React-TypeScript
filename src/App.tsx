ed386e7e374498c2ef12d395ea6363b26aaa05df
Syed Muhammed Bilal <intergo878@gmail.com>
2020-11-29 12:01:40 +0500

    new tyoes

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,30 @@
 import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+type FormElem = React.FormEvent<HTMLFormElement> // type for form 
 
-  const [value, setValue] = useState('');
+interface ITodo {
+  text: string;
+  completed: boolean;
+};
+
+const App:React.FC = () => {
+
+  const [value, setValue] = useState<string>(''); // always add generic in useState
+  const [todo, setTodo] = useState<ITodo[]>([]); 
+  // an interface contain types and at the end of the generic <ITodo> we add '[]' because it is an empty array
+
+  const handleSubmit = (e: FormElem):void => {
+    e.preventDefault();
+    setValue('')
+  }
 
   return (
     <React.Fragment>
       <h1>Todo App</h1>
-      <form>
-        <input type='text' />
-        <button type='submit'></button>
+      <form onSubmit={handleSubmit} >
+        <input value={value} type='text' onChange={e => setValue(e.target.value)} />
+        <button type='submit'>Add Todo</button>
       </form>
     </React.Fragment>
   )
